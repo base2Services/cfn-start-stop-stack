95106c438b7cc492c94cde52e0415384947a4c08
toshke <n.tosic@base2services.com>
2017-07-13 11:27:27 +1000

    Trim session name if too long

--- lib/aws_credentials.rb
+++ lib/aws_credentials.rb
@@ -7,11 +7,15 @@ module Base2
     def self.get_assume_role_credentials(session_name)
 
       #check if AWS_ASSUME_ROLE exists
+      session_name =  "#{session_name.gsub('_','-')}-#{Time.now.getutc.to_i}"
+      if session_name.length > 64
+        session_name = session_name[-64..-1]
+      end
       assume_role = ENV['AWS_ASSUME_ROLE'] or nil
       if not assume_role.nil?
         return Aws::AssumeRoleCredentials.new(
             role_arn: assume_role,
-            role_session_name: "#{session_name.gsub('_','-')}-#{Time.now.getutc.to_i}"
+            role_session_name: session_name
         )
       end
 
