6f01b150ac0c327b241b326c0b6816492ef75038
Guslington <guslington@gmail.com>
2019-01-15 12:05:20 +1100

    minor version bump

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -1,7 +1,7 @@
 require 'cfn_manage/version'
 
 describe 'Version' do
-  it 'is version 0.5.3' do
-    expect(CfnManage::VERSION).to eq("0.5.3")
+  it 'is version 0.5.4' do
+    expect(CfnManage::VERSION).to eq("0.5.4")
   end
 end
