5a3b67def69a3232e89bb1615d3e4cb7397daace
Blake Cramer <blakecramer@Blakes-MacBook.local>
2019-07-25 15:40:06 +1000

    wait added for start stop of sftp and gemspec adjustment for transfer

--- cfn_manage.gemspec
+++ cfn_manage.gemspec
@@ -26,6 +26,8 @@ Gem::Specification.new do |spec|
   spec.add_runtime_dependency 'aws-sdk-autoscaling', '~> 1', '<2'
   spec.add_runtime_dependency 'aws-sdk-ecs', '~> 1', '<2'
   spec.add_runtime_dependency 'aws-sdk-docdb', '>=1.9.0', '<2'
+  spec.add_runtime_dependency 'aws-sdk-transfer', '~>1', '<2'
+
 
   spec.add_development_dependency "bundler", "~> 2.0"
   spec.add_development_dependency "rake", "~> 10.0"
--- lib/cfn_manage/transfer_start_stop_handler.rb
+++ lib/cfn_manage/transfer_start_stop_handler.rb
@@ -1,4 +1,5 @@
 require 'cfn_manage/aws_credentials'
+require 'aws-sdk-transfer'
 
 module CfnManage
 
@@ -15,11 +16,11 @@ module CfnManage
     end
 
     def start(configuration)
-      
+
       state = get_state()
-      
+
       if state != "OFFLINE"
-        $log.warn("SFTP Server #{@server_id} is in a state of #{@state} and can not be started.")
+        $log.warn("SFTP Server #{@server_id} is in a state of #{state} and can not be started.")
         return
       end
 
@@ -27,26 +28,33 @@ module CfnManage
       @client.start_server({
         server_id: @server_id,
       })
+      unless @skip_wait
+        $log.info("Waiting for SFTP to start #{@cluster_id}")
+        wait('ONLINE')
+      end
 
       return configuration
     end
-    
+
     def stop()
-      
+
       state = get_state()
-      
+
       if state != "ONLINE"
-        $log.warn("SFTP Server #{@server_id} is in a state of #{@state} and can not be stopped.")
+        $log.warn("SFTP Server #{@server_id} is in a state of #{state} and can not be stopped.")
         return {}
       end
-          
+
       @client.stop_server({
         server_id: @server_id,
       })
-      
+      unless @skip_wait
+        $log.info("Waiting for SFTP to stop #{@cluster_id}")
+        wait('OFFLINE')
+      end
       return {}
     end
-    
+
     def get_state()
       resp = @client.describe_server({
         server_id: @server_id,
@@ -54,35 +62,35 @@ module CfnManage
       return resp.server.state
     end
 
-  end
-  
-  def wait(completed_state)
-    
-    state_count = 0
-    steady_count = 2
-    attempts = 0
-    until attempts == (max_attempts = 60*6) do
-      state = get_state()
-      $log.info("SFTP Server #{@server_id} state: #{state}, waiting for #{completed_state}")
+    def wait(completed_state)
 
-      if state == "#{completed_state}"
-        state_count = state_count + 1
-        $log.info("#{state_count}/#{steady_count}")
-      elsif ["START_FAILED", "STOP_FAILED"].inculde?(state)
-        $log.error("SFTP Server #{@server_id} failed to reach state #{completed_state} with current state #{state}!")
-        break
-      else
-        state_count = 0
+      state_count = 0
+      steady_count = 2
+      attempts = 0
+      until attempts == (max_attempts = 60*6) do
+        state = get_state()
+        $log.info("SFTP Server #{@server_id} state: #{state}, waiting for #{completed_state}")
+
+        if state == "#{completed_state}"
+          state_count = state_count + 1
+          $log.info("#{state_count}/#{steady_count}")
+        elsif ["START_FAILED", "STOP_FAILED"].include?(state)
+          $log.error("SFTP Server #{@server_id} failed to reach state #{completed_state} with current state #{state}!")
+          break
+        else
+          state_count = 0
+        end
+        break if state_count == steady_count
+        attempts = attempts + 1
+        sleep(15)
       end
-      break if state_count == steady_count
-      attempts = attempts + 1
-      sleep(15)
+
+      if attempts == max_attempts
+        $log.error("SFTP Server #{@server_id} did not enter #{completed_state} state, however continuing operations...")
+      end
+
     end
 
-    if attempts == max_attempts
-      $log.error("SFTP Server #{@server_id} did not enter #{completed_state} state, however continuing operations...")
-    end
-    
   end
-  
+
 end
