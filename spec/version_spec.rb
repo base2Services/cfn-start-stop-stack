b7f4983f87aacfd775fbedc2bb488f49c6db95c6
Guslington <guslington@gmail.com>
2019-10-30 13:53:27 +1100

    version bump to 0.8.1

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -1,7 +1,7 @@
 require 'cfn_manage/version'
 
 describe 'Version' do
-  it 'is version 0.8.0' do
-    expect(CfnManage::VERSION).to eq("0.8.0")
+  it 'is version 0.8.1' do
+    expect(CfnManage::VERSION).to eq("0.8.1")
   end
 end
