2adcefb85de6176f1320f7e72801f502a1d0f7ca
Guslington <guslington@gmail.com>
2022-11-03 20:27:59 +1100

    update test

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -1,7 +1,7 @@
 require 'cfn_manage/version'
 
 describe 'Version' do
-  it 'is version 0.8.3' do
-    expect(CfnManage::VERSION).to eq("0.8.3")
+  it 'is version 0.8.5' do
+    expect(CfnManage::VERSION).to eq("0.8.5")
   end
 end
